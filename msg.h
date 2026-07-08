468b8cb164b410bc866051c2ce5b4a5cee611736
Eduardo Costa <m4c0@users.noreply.github.com>
2026-07-08 11:45:47 +0100

    msg: loading messages

--- msg.h
+++ msg.h
@@ -21,10 +21,16 @@ msg_t msg_convo[10000] = {0};
 void msg_print_indented(FILE * f, const char * txt) {
   while (txt && *txt) {
     const char * c = strchr(txt, '\n');
+    if (c == txt) {
+      fprintf(f, "\n");
+      txt++;
+      continue;
+    }
     int len = c ? (int)(c - txt) : strlen(txt);
     fprintf(f, "  %.*s\n", (int)(c - txt), txt);
     txt = c;
   }
+  fprintf(f, ".\n");
 }
 
 int msg_save(const char * name) {
@@ -42,6 +48,7 @@ int msg_save(const char * name) {
       fprintf(f, "reas\n");
       msg_print_indented(f, m->reas);
     }
+    fprintf(f, "\n");
   }
   fclose(f);
 
@@ -49,7 +56,57 @@ int msg_save(const char * name) {
   return 0;
 }
 int msg_load(const char * name) {
-  printf("would load from %s\n", name);
+  FILE * f = fopen(name, "rb"); 
+  assert(f);
+
+  char buf[102400];
+  msg_t * m = NULL;
+  char * tgt = NULL;
+  unsigned tln = 0;
+  while (fgets(buf, sizeof(buf), f)) {
+    buf[strlen(buf) - 1] = 0;
+
+    if (strncmp(buf, "role ", 5) == 0) {
+      m = m ? m + 1 : msg_convo;
+      m->role = strdup(buf + 5);
+      tgt = NULL;
+      continue;
+    }
+    if (strncmp(buf, "call ", 5) == 0) { tgt = NULL; m->call = strdup(buf + 5); continue; }
+    if (strncmp(buf, "name ", 5) == 0) { tgt = NULL; m->name = strdup(buf + 5); continue; }
+    if (strncmp(buf, "fini ", 5) == 0) { tgt = NULL; m->fini = strdup(buf + 5); continue; }
+
+    if (strcmp(buf, "cont") == 0) { tln = 102400; tgt = m->cont = calloc(tln, 1); continue; }
+    if (strcmp(buf, "reas") == 0) { tln = 102400; tgt = m->reas = calloc(tln, 1); continue; }
+
+    if (!tgt) {
+      if (strlen(buf) == 0) continue; // ignore empty line outside multilines
+      fprintf(stderr, "invalid command: %s\n", buf);
+      return 1;
+    }
+    if (strlen(buf) == 0) {
+      assert(tln);
+      strncat(tgt++, "\n", tln--);
+      continue;
+    }
+    if (strcmp(buf, ".") == 0) {
+      tgt = NULL;
+      tln = 0;
+      continue;
+    }
+    if (strncmp(buf, "  ", 2) != 0) {
+      fprintf(stderr, "invalid indentation: [%s]\n", buf);
+      return 1;
+    }
+    int len = strlen(buf) - 2;
+    assert(tln >= len + 1);
+    strncpy(tgt, buf + 2, tln);
+    tgt += len;
+    tln -= len;
+  }
+  fclose(f);
+
+  printf("loaded from %s\n", name);
   return 0;
 }
 

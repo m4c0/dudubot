9135ee2874faf015ae3e3ad56dc5abd0e3e6776e
Eduardo Costa <m4c0@users.noreply.github.com>
2026-07-14 14:08:15 +0100

    some log utils

---
+++ log.h
@@ -0,0 +1,26 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+__attribute__((format(printf, 1, 2)))
+static inline int log_error(const char * msg, ...) {
+  va_list args;
+  va_start(args, msg);
+  vfprintf(stderr, msg, args);
+  va_end(args);
+  return 1;
+}
+
+__attribute__((format(printf, 1, 2)))
+static inline int log_info(const char * msg, ...) {
+  va_list args;
+  va_start(args, msg);
+  vfprintf(stderr, msg, args);
+  va_end(args);
+  return 0;
+}
+
+#endif
+
--- test-tool.c
+++ test-tool.c
@@ -1,12 +1,10 @@
+#include "log.h"
 #include "tll.h"
 
 #include <assert.h>
 
 int main(int argc, char ** argv) {
-  if (argc != 3) {
-    fprintf(stderr, "usage: %s <tool-name> <json>\n", argv[0]);
-    return 1;
-  }
+  if (argc != 3) return log_error("usage: %s <tool-name> <json>\n", argv[0]);
 
   tll_dudubot_exe = "./dudubot";
   if (tll_load(argv[1])) return 1;

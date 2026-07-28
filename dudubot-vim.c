66a8eb5375a7c7f3c17b80217445522b1b096ce4
Eduardo Costa <m4c0@users.noreply.github.com>
2026-07-28 23:42:02 +0100

    dudubot-vim: simpler interface

--- dudubot-vim.c
+++ dudubot-vim.c
@@ -22,37 +22,16 @@ void log_v(const char * msg, va_list args) {
 void wrt_log_cont(const char * str) {}
 void wrt_log_reas(const char * str) {}
 
-static char * vim_cmd;
-static int read_msg(const char * session) {
-  if (!vim_cmd) vim_cmd = malloc(1024000);
-  if (!fgets(vim_cmd, 1024000, stdin)) return 1;
-
-  json_array_element_t * arr = jsn_arr(json_parse(vim_cmd, strlen(vim_cmd)));
-  assert(arr);
-
-  arr = arr->next;
-  assert(arr);
-
-  const char * prompt = jsn_str(arr->value);
-  if (prompt) {
-    msg_alloc_user(prompt);
-    return cht_loop(session);
-  }
-
-  return 1;
-}
-
 int main(int argc, char ** argv) {
-  if (argc != 2) return log_error("Expected usage from vim: %s <chat-file>", argv[0]);
+  if (argc != 2) return log_error("Expected usage: %s <chat-file>", argv[0]);
 
   msg_load(argv[1]);
 
-  while (read_msg(argv[1])) {
-    printf("[\"ex\",\"edit %s\"]\n", argv[1]);
-    fflush(stdout);
-    printf("[\"redraw\",\"\"]\n");
-    fflush(stdout);
-  }
+  cht_loop(argv[1]);
+
+  printf("[\"ex\",\"edit %s\"]\n", argv[1]);
+  printf("[\"normal\",\"G\"]\n");
+  printf("[\"redraw\",\"\"]\n");
 
   tll_purge();
   return 0;

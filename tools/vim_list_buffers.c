2ad6c32b17ff9aacc500cda6df6e387292793ee2
Eduardo Costa <m4c0@users.noreply.github.com>
2026-07-29 08:54:28 +0100

    vim: support to list open buffers

--- tools/vim_list_buffers.c
+++ tools/vim_list_buffers.c
@@ -1,3 +1,4 @@
+#include "../jsn.h"
 #include "../tll_data.h"
 
 #include <stdio.h>
@@ -5,18 +6,25 @@
 
 static char * buf;
 static const char * exec(tll_call_t t) {
-  printf("[\"expr\",\"getbufinfo()\",-1]\n");
+  printf("[\"expr\",\"map(getbufinfo(), 'v:val.name')\",-1]\n");
+  fflush(stdout);
 
-  fgets(buf, 1024000, stdin);
+  if (!buf) buf = malloc(1024000);
+  if (!fgets(buf, 1024000, stdin)) return "tool failed to run";
 
+  json_value_t * root = json_parse(buf, strlen(buf));
+  json_array_element_t * arr = jsn_arr(root);
+  if (!arr) return "VIM returned a non-array message";
+
+  if (jsn_atoi(arr->value) != -1) return "VIM returned an invalid message";
+
+  free(root);
   return buf;
 }
 
 EXPORT void dudubot_tool(tll_api_t * api) {
   if (api->magic != TLL_API_MAGIC_IN) return;
   api->magic = TLL_API_MAGIC_OUT;
-
-  buf = malloc(1024000);
 
   *api->t = (tll_t) {
     .desc = "List open files.",
